f53d96924b5ec7a26396e238584805cd318e0cb9
Rico Roma <ricoroma14@gmail.com>
2020-08-05 12:33:10 -0300

    Layouts v1.0

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html class="h-100 w-100">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<title>Layouts Page</title>
+
+</head>
+<body class="w-100 h-100">
+
+	<div id="content" class="w-100 h-100 d-flex position-relative">
+		
+		<div class="row w-100 h-50 border rounded p-3 text-center">
+
+			<div class="col-sm-12 p-1">
+				<p class="font-weight-bold">Escolha o layout</p>
+			</div>
+			<div class="col-sm-4 p-2">
+				<a class="link" href="layout1">Layout 1</a>
+			</div>
+			<div class="col-sm-4 p-2">
+				<a class="link" href="layout2">Layout 2</a>
+			</div>
+			<div class="col-sm-4 p-2">
+				<a class="link" href="layout3">Layout 3</a>
+			</div>
+
+		</div>
+
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
